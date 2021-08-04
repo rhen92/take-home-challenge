9e80b1c039db884962e0b68b9e60ac5b5a484c0f
Rachel Hendricks <rachelhen92@gmail.com>
2021-08-04 09:11:09 -0600

    Delete files and edit App

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: []
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Hello</h1>
+      </main>
+    )
+  }
+}
+
+export default App;
