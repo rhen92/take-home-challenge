dcd0985b26a15840e04b011d31a656d627e4fc4a
Rachel Hendricks <rachelhen92@gmail.com>
2021-08-04 21:15:53 -0600

    Add in p tag when no synopsis is written

--- src/components/Spotlight/Spotlight.js
+++ src/components/Spotlight/Spotlight.js
@@ -7,12 +7,14 @@ const Spotlight = ({ title, findSelectedArticle }) => {
   return (
     <section>
       <Link to={'/'} className='back-button'>🔙</Link>
-      <h1>{articleWanted.title}</h1>
-      <h2>Published: {new Date(articleWanted.published_date).toDateString()}</h2>
-      <h3>{articleWanted.byline}</h3>
-      <img className='spotlight-image' src={!articleWanted.image ? <h3>Loading image...</h3> : articleWanted.image[0].url} alt={!articleWanted.image ? <h3>Loading image...</h3> : articleWanted.image[0].caption} />
-      <p>Synopsis: {articleWanted.abstract}</p>
-      <a href={articleWanted.url} target='_blank' rel='noreferrer'>Click here to read more</a>
+      <div className='article-wanted'>
+        <h1>{articleWanted.title}</h1>
+        <h2>Published: {new Date(articleWanted.published_date).toDateString()}</h2>
+        <h3>{articleWanted.byline}</h3>
+        <img className='spotlight-image' src={!articleWanted.image ? <h3>Loading image...</h3> : articleWanted.image[0].url} alt={!articleWanted.image ? <h3>Loading image...</h3> : articleWanted.image[0].caption} />
+        <p>Synopsis: {articleWanted.abstract === '' ? 'Sorry no synopsis was written' : articleWanted.abstract}</p>
+        <a href={articleWanted.url} target='_blank' rel='noreferrer'>Click here to read more</a>
+      </div>
     </section>
   )
 }
